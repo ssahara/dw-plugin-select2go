1336b75b6fc1c64e3e103d993ef91a7e715c6dc7
Satoshi Sahara <sahara.satoshi@gmail.com>
2013-12-16 23:05:58 +0900

    add lang setting files

---
+++ lang/en/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * English language file for DokuWiki Plugin Select2
+ * used in the configuration manager
+ */
+
+$lang['force_select2'] = "use always jQuery Select2 for select tag";
---
+++ lang/ja/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Japanese language file for DokuWiki Plugin Select2
+ * used in the configuration manager
+ */
+
+$lang['force_select2'] = "select タグに jQuery Select2 を適用する";
